d5d84e767e12e8fbab7132ed0d9feaf242545629
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2013-11-15 12:17:44 +0100

    get rid of duplicates

--- src/main/java/com/era7/lib/era7xmlapi/interfaces/IAttribute.java
+++
@@ -1,7 +0,0 @@
-package com.era7.lib.era7xmlapi.interfaces;
-
-public interface IAttribute extends IXmlThing{
-
-	org.jdom2.Attribute asJdomAttribute();
-	
-}
--- src/main/java/com/era7/lib/era7xmlapi/model/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * XML management utility classes
- */
-
-package com.era7.lib.era7xmlapi.model;
-
